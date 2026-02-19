8d315d3d9409922eae35cf6b49d2ccc81c775fe7
furkang-risa <Furkan@risa-it.nl>
2026-02-19 10:49:35 +0100

    add backend

---
+++ backend/hearty/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.2"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.gurbuz"
+version = "0.0.1-SNAPSHOT"
+description = "Backend for ticky"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-h2console")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.h2database:h2")
+	testImplementation("org.springframework.boot:spring-boot-starter-jdbc-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ backend/hearty/src/test/java/com/gurbuz/hearty/HeartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurbuz.hearty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
